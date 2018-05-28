c171205d75befca27d8c4e8ab67f839baa568646
Joshua Barretto <joshua.s.barretto@gmail.com>
2018-05-28 18:00:48 +0100

    Tabs to spaces

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,29 @@
 pub struct MacroChunk {
-	alt: u32,
+    alt: u32,
 }
 
 pub struct MacroWorld {
-	seed: u64,
-	size: u32,
-	chunks: Vec<MacroChunk>,
+    seed: u64,
+    size: u32,
+    chunks: Vec<MacroChunk>,
 }
 
 impl MacroWorld {
-	pub fn new(seed: u64, size: u32) -> MacroWorld {
-		let mut chunks = Vec::new();
+    pub fn new(seed: u64, size: u32) -> MacroWorld {
+        let mut chunks = Vec::new();
 
-		for i in 0..size * size {
-			chunks.push(MacroChunk {
-				alt: 0,
-			});
-		}
+        for i in 0..size * size {
+            chunks.push(MacroChunk {
+                alt: 0,
+            });
+        }
 
-		MacroWorld {
-			seed,
-			size,
-			chunks,
-		}
-	}
+        MacroWorld {
+            seed,
+            size,
+            chunks,
+        }
+    }
 }
 
 #[cfg(test)]
