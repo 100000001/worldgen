b118f1ccc57378598d4f660b34af63f163f42656
Joshua Barretto <joshua.s.barretto@gmail.com>
2018-05-28 17:48:51 +0100

    Added MacroWorld creation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
+pub struct MacroChunk {
+	alt: u32,
+}
+
 pub struct MacroWorld {
+	seed: u64,
 	size: u32,
+	chunks: Vec<MacroChunk>,
+}
+
+impl MacroWorld {
+	pub fn new(seed: u64, size: u32) -> MacroWorld {
+		let mut chunks = Vec::new();
+
+		for i in 0..size * size {
+			chunks.push(MacroChunk {
+				alt: 0,
+			});
+		}
+
+		MacroWorld {
+			seed,
+			size,
+			chunks,
+		}
+	}
 }
 
 #[cfg(test)]
